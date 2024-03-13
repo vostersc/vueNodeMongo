c99f6db209c7bcc7009044fc732040c7489be82d
vostersc <vosters01@gmail.com>
2024-03-12 22:40:41 -0600

    query finished

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const dbConnect = require('./db.js');
 const app = express();
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
 app.use(express.static('public'));
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(dbConnect);
 
 app.get('/', (r, res) => {
@@ -18,7 +19,6 @@ app.get('/customers/:customerId?', async (req, res) => {
 
         if(!req.params.customerId){
             const allCustomers = await collection.find({}).toArray();
-            console.log('index.js: 31 --->', allCustomers);
             res.json(allCustomers);
             return;
         }
@@ -30,8 +30,113 @@ app.get('/customers/:customerId?', async (req, res) => {
         console.log('index.js: 41 --->', err);
         res.send('error');
     }
-
 });
 
+app.delete('/customers/:customerId', async (req, res) => {
+    try {
+        if(!req.params.customerId) return res.status(422).json({error: 'Customer Id Required.'});
+
+        const collection = req.dbClient.db('database').collection('customers');
+        const result = await collection.deleteOne({ customerId: req.params.customerId });
+
+        if (result.deletedCount === 1) {
+            res.json({ message: "Customer deleted successfully" });
+        } else {
+            res.status(404).json({ error: "Customer not found" });
+        }
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+app.post('/logs', async (req, res) => {
+    try {
+        const { locationId, startDate, endDate } = req.body;
+        const hasAllValues = locationId && startDate && endDate;
+        if (!hasAllValues) return res.status(422).json({ error: 'Location ID, start date, and end date are required.' });
+        
+        const pipeline = [
+            {
+                $lookup: {
+                    from: "locations",
+                    localField: "locationId",
+                    foreignField: "locationId",
+                    as: "location"
+                }
+            },
+            { $match: { "location.locationId": locationId } },
+            {
+                $lookup: {
+                    from: "customerLogs",
+                    localField: "customerId",
+                    foreignField: "customerId",
+                    as: "logs"
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    customerData: "$$ROOT",
+                    location: { $arrayElemAt: ["$location", 0] },
+                    logs: {
+                        $filter: {
+                            input: "$logs",
+                            as: "log",
+                            cond: {
+                                $and: [ { $gte: ["$$log.date", makeISO(startDate)] }, { $lte: ["$$log.date", makeISO(endDate)] } ]
+                            }
+                        }
+                    }
+                }
+            }
+        ];
+        
+        const customersCollection = req.dbClient.db('database').collection('customers');
+        const allMatchingData = await customersCollection.aggregate(pipeline).toArray();
+        const cleanData = allMatchingData.map(customer => {
+            delete customer.location;
+            delete customer.customerData.logs
+            return customer;
+        });
+
+        res.send(cleanData);
+
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+
+    function makeISO(d){ return new Date(d).toISOString(); }
+});
 
 app.listen(process.env.SERVER_PORT, () => console.log(`Listening on ${process.env.SERVER_PORT}`));
+
+
+// try both of the below requests to see date range working
+    // POST TO: localhost:3001/logs
+    // {
+    //     "locationId": "1",
+    //     "startDate": "2024-03-10T01:00:00.000+00:00",
+    //     "endDate": "2040-03-09T09:00:00.000+00:00"
+    // }
+
+    // POST TO: localhost:3001/logs
+    // {
+    //     "locationId": "1",
+    //     "startDate": "2024-03-11T01:00:00.000+00:00",
+    //     "endDate": "2040-03-09T09:00:00.000+00:00"
+    // }
+
+
+
+
+
+
+//this request shows location filtering working
+    // POST TO: localhost:3001/logs
+    // {
+    //     "locationId": "2",
+    //     "startDate": "2020-03-10T01:00:00.000+00:00",
+    //     "endDate": "2040-03-09T09:00:00.000+00:00"
+    // }
--- public/customer-list.js
+++ public/customer-list.js
@@ -52,6 +52,19 @@ Vue.component('customer-list', {
           });
         }
       },
+      deleteCustomer(customerId) {
+        // Assuming you have an endpoint to delete a customer
+        axios.delete(`/customers/${customerId}`)
+          .then(response => {
+            console.log('customer-list.js: 60 --->', response);
+            if(response.error) throw new Error('deletion error');
+
+            this.fetchCustomers();
+          })
+          .catch(error => {
+            console.error('Error deleting customer:', error);
+          });
+      },
       formatPhoneNumber(phone) {
         return phone.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
       }
@@ -62,63 +75,74 @@ Vue.component('customer-list', {
       }
     },
     template: `
-      <div>
-        <input type="text" v-model="filterText" placeholder="Filter customers">
+      <div
+        style="display: flex; flex-direction: column; justify-content: center; padding: 8px; padding-bottom: 4px; border: 1px solid black; width: 674px; height: 185px;"  
+      >
+        <input
+            type="text" v-model="filterText" placeholder="Search"
+            style="width: 150px; padding: 4px; margin-bottom: 4px;"
+        >
         <table>
           <thead>
             <tr>
                 <th
                     @click="sortByColumn('firstName')"
-                    style="cursor: pointer; min-width: 150px;"
+                    style="cursor: pointer; min-width: 150px; text-align: left;"
                 >
                     First Name
                 </th>
                 <th
                     @click="sortByColumn('lastName')"
-                    style="cursor: pointer; min-width: 150px;"
+                    style="cursor: pointer; min-width: 150px; text-align: left;"
                 >
                     Last Name
                 </th>
                 <th
                     @click="sortByColumn('phone')"
-                    style="cursor: pointer; min-width: 150px;"
+                    style="cursor: pointer; min-width: 150px; text-align: left;"
                 >
                     Phone
                 </th>
                 <th
                     @click="sortByColumn('email')"
-                    style="cursor: pointer; min-width: 150px;"
+                    style="cursor: pointer; min-width: 150px; text-align: left;"
                 >
                     Email
                 </th>
                 <th
-                    style="min-width: 150px;"
+                    style="min-width: 150px; color: 'red'; text-align: left;"
                 >
-                    Action
                 </th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="customer in filteredCustomers" :key="customer._id">
-              <td
-                style="min-width: 150px; text-align: center;"
-              >
-                {{ customer.firstName }}</td>
-              <td
-                style="min-width: 150px; text-align: center;"
-              >
-                {{ customer.lastName }}</td>
-              <td
-                style="min-width: 150px; text-align: center;"
-              >
-                {{ customer.phone }}</td>
-              <td
-                style="min-width: 150px; text-align: center;"
-              >
-                {{ customer.email }}</td>
-              <td
-                style="min-width: 150px; text-align: center;"
-              ><!-- Action column content --></td>
+                <tr v-for="customer in filteredCustomers" :key="customer._id">
+                    <td
+                        style="min-width: 150px;"
+                    >
+                        {{ customer.firstName }}</td>
+                    <td
+                        style="min-width: 150px;"
+                    >
+                        {{ customer.lastName }}</td>
+                    <td
+                        style="min-width: 150px;"
+                    >
+                        {{ customer.phone }}</td>
+                    <td
+                        style="min-width: 150px;"
+                    >
+                        {{ customer.email }}</td>
+                    <td
+                        style="min-width: 150px;"
+                    >
+                    <button
+                        @click="deleteCustomer(customer.customerId)"
+                        style="cursor: pointer; margin-bottom: 4px;"
+                    >
+                        delete
+                    </button>
+                </td>
             </tr>
           </tbody>
         </table>
